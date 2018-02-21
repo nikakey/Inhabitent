a76e2cc1c46fd1ef9ccbf81ad361c770e428a810
Veronika Kochetkova <veronica90k@gmail.com>
2018-02-21 10:00:50 -0800

    Resize icons in the footer

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -13,12 +13,12 @@
 				<div class="site-info">
 					<div class="contact-info">
 						<h3>Contact Info</h3>
-						<p><i class="fas fa-envelope"></i><a href="#">info@inhabitent.com</a></p>
+						<p><i class="fas fa-envelope" data-fa-transform="grow-5"></i><a href="#">info@inhabitent.com</a></p>
 						<p><i class="fas fa-phone" data-fa-transform="rotate-90"></i><a href="#">778-456-7891</a></p>
 						<ul class="social">
-							<li><i class="fab fa-facebook-square"></i></li>
-							<li><i class="fab fa-twitter-square"></i></li>
-							<li><i class="fab fa-google-plus-square"></i></li>
+							<li><i class="fab fa-facebook-square" data-fa-transform="grow-4"></i></li>
+							<li><i class="fab fa-twitter-square" data-fa-transform="grow-4"></i></li>
+							<li><i class="fab fa-google-plus-square" data-fa-transform="grow-4"></i></li>
 						</ul>
 					</div>
 					<div class="business-hours">
