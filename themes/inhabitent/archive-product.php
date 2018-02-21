e67fb42e8d88da54e5013b7ef4f779a05b785b41
Veronika Kochetkova <veronica90k@gmail.com>
2018-02-21 09:50:51 -0800

    Add social icons in the footer

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying archive pages.
+ * The template for displaying archive product page.
  *
  * @package Inhabitent_Theme
  */
--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -13,9 +13,13 @@
 				<div class="site-info">
 					<div class="contact-info">
 						<h3>Contact Info</h3>
-						<p><i class="fas fa-envelope"></i><a href="#"> info@inhabitent.com</a></p>
-						<p><a href="#">778-456-7891</a></p>
-						<p></p>
+						<p><i class="fas fa-envelope"></i><a href="#">info@inhabitent.com</a></p>
+						<p><i class="fas fa-phone" data-fa-transform="rotate-90"></i><a href="#">778-456-7891</a></p>
+						<ul class="social">
+							<li><i class="fab fa-facebook-square"></i></li>
+							<li><i class="fab fa-twitter-square"></i></li>
+							<li><i class="fab fa-google-plus-square"></i></li>
+						</ul>
 					</div>
 					<div class="business-hours">
 						<h3>Business Hours</h3>
