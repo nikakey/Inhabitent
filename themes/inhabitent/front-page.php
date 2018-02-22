5147d036da475ba1d4722e5995a3962d53fa5d46
Veronika Kochetkova <veronica90k@gmail.com>
2018-02-21 17:51:14 -0800

    Style the Shop Stuff section of the front page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -42,10 +42,9 @@ get_header(); ?>
 									'/images/product-type-icons/' . $term->slug; ?>.svg"
 									alt="<?php echo $term->name; ?>"/>
 								<p><?php echo $term->description; ?></p>
-								<p>
-									<a href="<?php echo get_term_link( $term ); ?>"
-									class="btn"><?php echo $term->name; ?> Stuff</a>
-								</p>
+								<button href="<?php echo get_term_link( $term ); ?>">
+									<?php echo $term->name; ?> Stuff
+								</button>
 							</div>
 						<?php endforeach; ?>
 					</div>
