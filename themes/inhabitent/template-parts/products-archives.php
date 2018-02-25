8d91148c186d922abead6ab5fd9615d761242ba1
Veronika Kochetkova <veronica90k@gmail.com>
2018-02-25 15:17:47 -0800

    Style product item cells on the Shop page

--- themes/inhabitent/template-parts/products-archives.php
+++ themes/inhabitent/template-parts/products-archives.php
@@ -5,11 +5,10 @@
         <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'medium_large' ); ?></a>
       </div>
     <?php endif; ?>
-
-    <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', 
-    esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-    <?php echo CFS()->get( 'price' ); ?>
+    <div class="content-products-wrapper">
+      <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+      <span class="price"><?php echo CFS()->get( 'price' ); ?></span>
+    </div>
 
   </header><!-- .entry-header -->
 
