2e8ac879c62b7e500d33176c9f1076e6c52b5532
Veronika Kochetkova <veronica90k@gmail.com>
2018-03-08 19:43:18 -0800

    Add permalinks to the titles of adventure items and to the button links on the Adventures Archive page

--- themes/inhabitent/template-parts/adventures-archives.php
+++ themes/inhabitent/template-parts/adventures-archives.php
@@ -2,10 +2,10 @@
     
     <?php if ( has_post_thumbnail() ) : ?>
       <div class="image-wrapper">
-        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'full' ); ?></a>
+        <?php the_post_thumbnail( 'full' ); ?>
       </div>
     <?php endif; ?>
-    <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-    <button class="white-btn">Read More</button></a>
+    <a href="<?php the_permalink(); ?>"><?php the_title( '<h2 class="entry-title">', '</h2>' ); ?></a>
+    <a class="white-btn" href="<?php the_permalink(); ?>">Read More</a>
 
 </article><!-- #post-## -->
